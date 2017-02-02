b8d0b52a357e1e1f6a15f120d32d48d6dd205264
Simon Conseil <contact@saimon.org>
2017-02-02 17:18:21 +0100

    Build shared library after the utils binaries

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,14 @@
 set -e
 
 ./configure --prefix=$PREFIX --enable-reentrant || { cat config.log ; exit 1 ; }
-make stand_alone shared utils
+make stand_alone utils
 
 # test-ish programs:
 ./cookbook
 ./speed
 ./testprog
 
+make shared
 make install
 
 # NOTE: don't remove .a files! That's all we provide!
