422b7cfaaaa4d00cfe43caf36fc883c4a44c8df0
Sam Gillingham <gillingham.sam@gmail.com>
2019-05-13 16:22:13 +1000

    add Windows support

---
+++ recipe/bld.bat
@@ -0,0 +1,22 @@
+
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+  -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -D CMAKE_BUILD_TYPE=Release ^
+  ..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+:: test-ish programs:
+cookbook
+if errorlevel 1 exit 1
+testprog
+if errorlevel 1 exit 1
+  
+nmake install
+if errorlevel 1 exit 1
