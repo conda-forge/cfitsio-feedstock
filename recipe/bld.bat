aa32d9b695e994dd94877014105f3c8f5b22b1a8
Teake Nutma <t.a.nutma@rug.nl>
2022-03-18 15:59:58 +0100

    Don't move cfitsio.dll
    See https://github.com/conda-forge/cfitsio-feedstock/pull/34#issuecomment-1069677973

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -23,10 +23,6 @@ if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
 
-:: DLL seems to go in wrong place
-move %LIBRARY_LIB%\cfitsio.dll %LIBRARY_BIN%
-if errorlevel 1 exit 1
-
 :: delete in case this breaks other things - only needed for compilation anyway
 del %LIBRARY_INC%\unistd.h
 if errorlevel 1 exit 1
