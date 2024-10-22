642828ef4a54700b6ccdfd491607c093753e3f0e
Peter Williams <peter@newton.cx>
2024-10-21 22:17:27 -0400

    Switch non-Windows builds to CMake too; drop Fortran dep
    Version 4.5.0 switches the build system to fully embrace the autotools
    and libtool. If we need to change the build system, we might as well
    jump to CMake, which is what the Windows builds are already using.
    
    The CMake build doesn't actually do anything with a Fortran compiler,
    and upon investigation the Autotools build hardly does either. The only
    Fortran source files are demos, and the Fortran support in the C library
    can be compiled without actually using the Fortran compiler; the Fortran
    compiler is just used to figure out what kind of name mangling to use.
    This scheme appears to default sensibly, and there's also the fact that
    there probably aren't any packages in conda-forge that are interfacing
    with CFITSTIO *in Fortran*. So, let's just drop the dependency on the
    Fortran compiler.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,33 +1,35 @@
 #! /bin/bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
-set -ex
+set -xeuo pipefail
+
+extra_cmake_args=(
+    -GNinja
+    -DTESTS=On
+    -DUTILS=On
+    -DUSE_BZIP2=On
+    -DUSE_CURL=On
+    -DUSE_PTHREADS=On
+)
 
 if [ $(uname) = Darwin ] ; then
     # Needed to get 'union semun' definition used in drvrsmem.c:
-    export CFLAGS="$CFLAGS -D_DARWIN_C_SOURCE"
-    export LDFLAGS="$LDFLAGS -Wl,-reexport_library,$PREFIX/lib/libz.dylib"
+    export CXXFLAGS="$CXXFLAGS -D_DARWIN_C_SOURCE"
 fi
 
-./configure --prefix=$PREFIX --with-bzip2 --enable-reentrant || { cat config.log ; exit 1 ; }
+# The Autotools-based build supports a few additional features, like support for
+# Globus gsiftp. Unless we decide that we need one of those, the CMake build is
+# maintained and is nicer to deal with.
 
-make shared utils
-make install
+mkdir build
+cd build
+cmake ${CMAKE_ARGS} "${extra_cmake_args[@]}" $SRC_DIR
+ninja -v
+ninja install
 
 if [ ${CONDA_BUILD_CROSS_COMPILATION:-0} -eq 0 ] ; then
-    # Test-ish programs:
-    $PREFIX/bin/cookbook
-    $PREFIX/bin/speed
-    # Actual test suite as described in docs/cfitsio.doc
-    $PREFIX/bin/testprog > testprog.lis
-    diff testprog.lis testprog.out
-    cmp testprog.fit testprog.std
-fi
+    ./cookbook
 
-rm -f $PREFIX/bin/cookbook $PREFIX/bin/speed $PREFIX/bin/testprog
-
-# check symbol exports on osx
-if [ $(uname) = Darwin ]; then
-    ${OTOOL} -l $PREFIX/lib/libcfitsio.dylib | grep "LC_REEXPORT_DYLIB"
+    ./TestProg >testprog.lis
+    diff testprog.lis ../testprog.out
+    cmp testprog.fit ../testprog.std
 fi
